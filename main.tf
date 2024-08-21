0fdb04ac1a20c4526033ed5766253b62551ba3f7
Bui Hong Nhat <87702834+ulsyou@users.noreply.github.com>
2024-08-21 00:53:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,26 +8,22 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3         = "http://localhost:4566"
-    cloudfront = "http://localhost:4566"
+    s3 = "http://localhost:4566"
   }
 }
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "my-website-bucket"
-  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
 }
 
-resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_object" "index_html" {
@@ -35,51 +31,4 @@ resource "aws_s3_object" "index_html" {
   key    = "index.html"
   source = "index.html"
   acl    = "public-read"
-}
-
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.website_bucket.bucket_domain_name
-    origin_id   = aws_s3_bucket.website_bucket.bucket
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.website_bucket.bucket
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-  tags = {
-    Name = "my-cloudfront-distribution"
-  }
-}
-
-output "cloudfront_url" {
-  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
